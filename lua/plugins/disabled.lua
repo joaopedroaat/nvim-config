048fbbc0e9fcd457dd51ea12778c37561776486b
João Pedro <joaopedroaat@outlook.com>
2024-09-19 01:30:06 -0300

    Disable noice and nvim-notify

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,4 +3,6 @@ return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
   { "folke/persistence.nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
 }
