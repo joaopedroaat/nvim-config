e3cdd8f12c1919e165165d4633b30afa9b230807
João Pedro <joaopedroaat@proton.me>
2026-01-15 20:24:46 -0300

    Add conform

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,47 @@
+return {
+  'stevearc/conform.nvim',
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      go = { "goimports_reviser" },
+
+    },
+    format_on_save = function(bufnr)
+      -- Disable autoformat on certain filetypes
+      local ignore_filetypes = { "sql", "java" }
+      if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+        return
+      end
+      -- Disable with a global or buffer-local variable
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+      -- Disable autoformat for files in a certain path
+      local bufname = vim.api.nvim_buf_get_name(bufnr)
+      if bufname:match("/node_modules/") then
+        return
+      end
+      -- ...additional logic...
+      return { timeout_ms = 500, lsp_format = "fallback" }
+    end,
+  },
+  init = function()
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = "Disable autoformat-on-save",
+      bang = true,
+    })
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = "Re-enable autoformat-on-save",
+    })
+  end
+}
