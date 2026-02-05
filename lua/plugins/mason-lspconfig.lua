0c59ac318b34ab01be9480219b17aea91617284d
João Pedro <joaopedroaat@proton.me>
2026-02-05 18:00:04 -0300

    Enable htmx lsp manually

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -3,6 +3,12 @@ return {
 	opts = {
 		ensure_installed = { "lua_ls" },
 	},
+	config = function(_, opts)
+		require("mason-lspconfig").setup(opts)
+
+		-- Mason installs a broken version of htmx, install it manually!
+		vim.lsp.enable("htmx")
+	end,
 	dependencies = {
 		{ "mason-org/mason.nvim", opts = {} },
 		"neovim/nvim-lspconfig",
