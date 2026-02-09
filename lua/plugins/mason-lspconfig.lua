c44b8a05e49e18ed47e9964c48648a0d0f67aaf7
João Pedro <joaopedroaat@proton.me>
2026-02-09 08:52:17 -0300

    Make mason-lspconfig not lazy / Remove htmx lsp enable by default

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,13 +1,11 @@
 return {
 	"mason-org/mason-lspconfig.nvim",
+	lazy = false,
 	opts = {
 		ensure_installed = { "lua_ls" },
 	},
 	config = function(_, opts)
 		require("mason-lspconfig").setup(opts)
-
-		-- Mason installs a broken version of htmx, install it manually!
-		vim.lsp.enable("htmx")
 	end,
 	dependencies = {
 		{ "mason-org/mason.nvim", opts = {} },
