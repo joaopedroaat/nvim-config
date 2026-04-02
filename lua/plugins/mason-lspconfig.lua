5c7dee24b47a9c43e3b6ca98e6af87db20f6e2eb
João Pedro <joaopedroaat@proton.me>
2026-04-02 11:38:12 -0300

    auto-download formatters with mason-tool-installer

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,16 +1,46 @@
 return {
 	"mason-org/mason-lspconfig.nvim",
-	lazy = false,
-	opts = {
-		ensure_installed = { "lua_ls" },
-	},
-	config = function(_, opts)
-		require("mason-lspconfig").setup(opts)
-	end,
 	dependencies = {
 		{ "mason-org/mason.nvim", opts = {} },
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
 		"neovim/nvim-lspconfig",
 	},
+	config = function()
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
+
+		-- 1. Setup Mason base
+		require("mason").setup()
+
+		-- 2. LSPs
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"lua_ls",
+				"gopls",
+				"templ",
+				"cssls",
+				"tailwindcss",
+				"ts_ls",
+				"html",
+				"jsonls",
+				"taplo",
+				"docker_language_server",
+				"lemminx",
+			},
+		})
+
+		-- 3. Other tools
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"stylua",
+				"goimports-reviser",
+				"prettierd",
+				"nixfmt",
+				"templ",
+			},
+		})
+	end,
+	lazy = false,
 	keys = {
 		{ "<leader>m", ":Mason<cr>", desc = "Mason" },
 	},
